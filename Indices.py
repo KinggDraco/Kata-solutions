8c879703957547345902c3a4305d9caab79b9d62
Draco <82430930+KinggDraco@users.noreply.github.com>
2022-04-21 20:41:23 -0700

    Create Indices.py

---
+++ Indices.py
@@ -0,0 +1,19 @@
+def head(numbers):
+    ans = numbers[0]
+    return ans
+
+def tail(numbers):
+    ans = numbers[1:]
+    return ans
+
+
+
+def init(numbers):
+    ans = numbers[:-1]
+
+    return ans
+
+
+def last(numbers):
+    ans = numbers[-1]
+    return ans
